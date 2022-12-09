2baa5430122badfb72077ba7b53e123fcb05bfef
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-12-09 08:02:47 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,7 @@
+#ifndef PTIMALIBRERIA_PRIMALIBRERIA_H
+#define PRIMALIBRERIA_PRIMALIBRERIA_H
 
+bool letteraono(int valore);
+char conversionelettere(vhar valore);
+
+#endif //PRIMALIBRERIA_PRIMALIBREIA_H
