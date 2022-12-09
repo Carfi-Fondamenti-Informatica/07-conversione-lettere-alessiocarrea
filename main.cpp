bea6ccf353223f334298391764b02ed3b44d625a
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-12-09 08:06:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char valore;
+  char esito;
+  cin >> valore;
+  if(letteraono(valore)==true){
+    esito=conversionelettere(valore);
+    cout << esito;
+  }
+  else if (letteraono(valore)==false){
+    cout << "errore" << endl;
+  }  
   return 0;
 }
