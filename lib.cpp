42dd04d7ec0cdb6b9df362372a1b2c55fb671708
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-12-09 07:25:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+#include "lib.h"
 
+bool letteraono(int valore){
+  if(valore>=65 && valore<=90 || valore>=97 && valore<=122){
+    return true;
+  }
+  else
+    return false;
+}
+
+char conversionelettere(char valore){
+  char esito;
+  if( valore>='A' && valore<='Z'){
+    esito=valore+32;
+  }
+  else if(valore>='a' && valore<='z'){
+    esito=valore-32;
+  }
+  return esito;
+}  
